ec85cfe578e3c200ef3b7b279522e3308eb79672
SuyeonSun <suninisy@gmail.com>
2023-04-14 11:28:55 +0900

    post controller, service, repository 로직 구현

--- src/main/java/com/example/springBootAws/domain/posts/PostsRepository.java
+++ src/main/java/com/example/springBootAws/domain/posts/PostsRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // 데이터베이스 접근하게 해줄 레파지토리
 // JpaRepository<Entity 클래스, pk 타입>
 // Entity 클래스와 기본 Entity 레파지토리는 함께 위치해야 함. Entity 클래스 역시 기본 레파지토리 없이는 제대로 역할x
-public interface PostRepository extends JpaRepository<Posts, Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 
 }
---
+++ src/main/java/com/example/springBootAws/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,32 @@
+package com.example.springBootAws.web.dto;
+
+import com.example.springBootAws.domain.posts.Posts;
+import lombok.Builder;
+
+// Entity 클래스를 Request/Response 클래스로 사용 지양
+// Entity 클래스를 기준으로 테이블과 스키마가 생성됨.
+// Entity 클래스가 변경되면 여러 클래스에 영향을 주지만,
+// Request/Response DTO는 view를 위한 클래스라 자주 변경이 필요함.
+// 따라서, View Layer과 DB Layer은 역할 분리를 철저히 하는 것이 중요.
+// 즉, Entity 클래스와 Controller에서 쓸 DTO는 분리해야 함.
+
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
