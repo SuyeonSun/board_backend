3b280356c5ea91f1426757b6beffca88f5a4c53d
SuyeonSun <suninisy@gmail.com>
2023-04-17 16:09:26 +0900

    생성 및 수정 날짜 저장 안되는 버그 수정

--- src/main/java/com/example/springBootAws/domain/BaseTimeEntity.java
+++ src/main/java/com/example/springBootAws/domain/BaseTimeEntity.java
@@ -1,14 +1,17 @@
 package com.example.springBootAws.domain;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass // Entity 클래스들이 BaseTimeEntity를 상속할 경우, 필드들(createdDate, modifiedDate)도 칼럼으로 인식하도록 함
+@EntityListeners(AuditingEntityListener.class) // Auditing 기능
 public abstract class BaseTimeEntity { // 모든 Entity의 상위 클래스가 되어 Entity들의 createdDate, modifiedDate를 관리
     @CreatedDate // Entity가 생성되어 저장될 때 시간이 자동 저장됨
     private LocalDateTime createdDate;
--- src/main/java/com/example/springBootAws/web/dto/PostsListResponseDto.java
+++ src/main/java/com/example/springBootAws/web/dto/PostsListResponseDto.java
@@ -10,12 +10,16 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
+
+    private LocalDateTime createdDate;
+
     private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
+        this.createdDate = entity.getCreatedDate();
         this.modifiedDate = entity.getModifiedDate();
     }
 }
